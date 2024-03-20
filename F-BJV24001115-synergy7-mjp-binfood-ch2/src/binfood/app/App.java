772653e56aba92e963e1af61afbecbc612eef2a1
Marcel Joshua <marceljoshua69@gmail.com>
2024-03-20 16:58:46 +0700

    optimize boilerplate

--- F-BJV24001115-synergy7-mjp-binfood-ch2/src/binfood/app/App.java
+++ F-BJV24001115-synergy7-mjp-binfood-ch2/src/binfood/app/App.java
@@ -10,14 +10,17 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 @SuppressWarnings("SpellCheckingInspection")
 public class App {
+
+    private static final int EXIT = 0;
+    private static final int CONFIRM_AND_PAY = 99;
 
     private final MenuItemService menuItemService;
 
@@ -29,51 +32,21 @@ public class App {
         seed();
 
         Map<MenuItem, Integer> orders = new HashMap<>();
-        Scanner scanner = new Scanner(System.in);
-        int choice;
 
-        while (true) {
-            mainMenu();
+        try (Scanner scanner = new Scanner(System.in)) {
+            int choice;
+            do {
+                mainMenu();
+                choice = getUserChoice(scanner);
 
-            System.out.print("=> ");
-            choice = scanner.nextInt();
-            System.out.println();
-
-            if (choice == 0)
-                break;
-            else if (choice == 99) {
-                confirmOrders(orders);
-
-                System.out.print("=> ");
-                int action = scanner.nextInt();
-
-                if (action == 0)
-                    break;
-                else if (action == 1) {
-                    generateReceipt(orders);
-                    break;
+                if (choice == CONFIRM_AND_PAY) {
+                    confirmOrder(orders);
+                    choice = handleConfirmation(orders, scanner);
+                } else if (isValidMenuItemChoice(choice)) {
+                    handleMenuItemChoice(orders, scanner, choice);
                 }
-
-            } else if (choice > 0 && choice <= menuItemService.size()) {
-                System.out.println("=".repeat(26));
-                System.out.println("Berapa pesanan anda");
-                System.out.println("=".repeat(26));
-
-                MenuItem menuItem = menuItemService.findById((long) choice);
-                System.out.printf("%-15s | %s\n", menuItem.getName(), Styler.currency(menuItem.getPrice()));
-                System.out.println("(input 0 untuk kembali)\n");
-                System.out.print("qty => ");
-                int quantity = scanner.nextInt();
-                System.out.println();
-
-                if (quantity > 0) {
-                    orders.put(menuItem, quantity);
-                }
-                /* if quantity is 0, do nothing (automatically goes back to menu) */
-            }
+            } while (choice != EXIT);
         }
-
-        scanner.close();
     }
 
     private void seed() {
@@ -82,6 +55,54 @@ public class App {
         menuItemService.addMenuItem("Nasi + Ayam", 18000);
         menuItemService.addMenuItem("Es Teh Manis", 3000);
         menuItemService.addMenuItem("Es Jeruk", 5000);
+    }
+
+    public void confirmOrder(Map<MenuItem, Integer> orders) {
+        System.out.println("=".repeat(26));
+        System.out.println("Konfirmasi & Pembayaran");
+        System.out.println("=".repeat(26) + "\n");
+
+        System.out.println(invoice(orders));
+
+        System.out.println("\n1. Konfirmasi dan Bayar");
+        System.out.println("2. Kembali ke menu utama");
+        System.out.println("0. Keluar aplikasi\n");
+    }
+    
+    private int getUserChoice(Scanner scanner) {
+        System.out.print("=> ");
+        return scanner.nextInt();
+    }
+
+    private boolean isValidMenuItemChoice(int choice) {
+        return choice > 0 && choice <= menuItemService.size();
+    }
+
+    private void handleMenuItemChoice(Map<MenuItem, Integer> orders, Scanner scanner, int choice) {
+        MenuItem menuItem = menuItemService.findById((long) choice);
+        int quantity = getQuantityFromUser(scanner, menuItem);
+        if (quantity > 0) {
+            orders.put(menuItem, quantity);
+        }
+    }
+
+    private int handleConfirmation(Map<MenuItem, Integer> orders, Scanner scanner) {
+        int action = getUserChoice(scanner);
+        if (action == 1) {
+            generateReceipt(orders);
+            return EXIT;
+        }
+        return action;
+    }
+
+    private int getQuantityFromUser(Scanner scanner, MenuItem menuItem) {
+        System.out.println("=".repeat(26));
+        System.out.println("How many would you like to order?");
+        System.out.println("=".repeat(26));
+        System.out.printf("%-15s | %s\n", menuItem.getName(), Styler.currency(menuItem.getPrice()));
+        System.out.println("(Enter 0 to return)\n");
+        System.out.print("qty => ");
+        return scanner.nextInt();
     }
 
     private void mainMenu() {
@@ -125,52 +146,42 @@ public class App {
         return invoiceContent.toString();
     }
 
-    private void confirmOrders(Map<MenuItem, Integer> orders) {
-        System.out.println("=".repeat(26));
-        System.out.println("Konfirmasi & Pembayaran");
-        System.out.println("=".repeat(26) + "\n");
-
-        System.out.println(invoice(orders));
-
-        System.out.println("\n1. Konfirmasi dan Bayar");
-        System.out.println("2. Kembali ke menu utama");
-        System.out.println("0. Keluar aplikasi\n");
-    }
-
     public void generateReceipt(Map<MenuItem, Integer> orders) {
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
-        Date currentDate = new Date();
-        String fileName = String.format("receipt_%s.txt",
-                formatter.format(currentDate));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
+        String fileName = String.format("receipt_%s.txt", LocalDateTime.now().format(formatter));
         Path dir = Paths.get("./receipt");
 
         try {
-            if (!Files.exists(dir)) {
-                Files.createDirectories(dir);
-            }
+            Files.createDirectories(dir);
 
             Path filePath = dir.resolve(fileName);
 
             try (BufferedWriter writer = Files.newBufferedWriter(filePath)) {
-                writer.write("=".repeat(26) + "\n");
-                writer.write("BinarFud" + "\n");
-                writer.write("=".repeat(26) + "\n");
-
-                writer.write("\nTerima kasih sudah memesan");
-                writer.write("\ndi BinarFud\n");
-                writer.write("\nDibawah ini adalah pesanan anda\n\n");
-
+                writer.write(receiptHeader());
                 writer.write(invoice(orders));
-
-                writer.write("\nPembayaran : BinarCash\n\n");
-                writer.write("=".repeat(26) + "\n");
-                writer.write("Simpan struk ini sebagai" + "\n");
-                writer.write("bukti pembayaran" + "\n");
-                writer.write("=".repeat(26) + "\n");
+                writer.write(receiptFooter());
             }
 
         } catch (IOException e) {
             System.err.printf("Error writing to %s: %s%n", fileName, e.getMessage());
         }
+    }
+
+    private String receiptHeader() {
+        return "=".repeat(26) + "\n" +
+                "BinarFud\n" +
+                "=".repeat(26) + "\n" +
+                "\nTerima kasih sudah memesan" +
+                "\ndi BinarFud\n" +
+                "\nDibawah ini adalah pesanan anda\n\n";
+    }
+
+    private String receiptFooter() {
+
+        return "\nPembayaran : BinarCash\n\n" +
+                "=".repeat(26) + "\n" +
+                "Simpan struk ini sebagai\n" +
+                "bukti pembayaran\n" +
+                "=".repeat(26) + "\n";
     }
 }
